d93f6d7bbf0667d8c1504ea342ee428f28cb371f
Rafael <rortizr@unbosque.edu.co>
2020-09-10 15:15:17 -0500

    Arrow functions

--- Seccion2:TypeScript_ES6/typescript/typescript/app.js
+++ Seccion2:TypeScript_ES6/typescript/typescript/app.js
@@ -1,14 +1,21 @@
 "use strict";
 (function () {
-    //parameters order: Obligatory, optionals, default value
-    function activate(who, moment, object) {
-        if (object === void 0) { object = 'batsignal'; }
-        if (moment) {
-            console.log(who + " activate the " + object + " at the " + moment);
+    var myFunction = function (a) {
+        return a;
+    };
+    var sumN = function (a, b) {
+        return a + b;
+    };
+    var myFunctionArrow = function (a) { return a; };
+    var sumA = function (a, b) { return a + b; };
+    var hulk = {
+        name: 'hulk',
+        smash: function () {
+            var _this = this;
+            setTimeout(function () {
+                console.log(_this.name + " smash!!!");
+            }, 1000);
         }
-        else {
-            console.log(who + " activate the " + object);
-        }
-    }
-    activate('Gordon', 'night');
+    };
+    hulk.smash();
 })();
