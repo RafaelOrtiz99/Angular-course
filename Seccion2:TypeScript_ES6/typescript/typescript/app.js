7ab2812c996e8604cb98a0df4f482b97b2a8ed8c
Rafael <rortizr@unbosque.edu.co>
2020-09-10 13:47:00 -0500

    parameters

--- Seccion2:TypeScript_ES6/typescript/typescript/app.js
+++ Seccion2:TypeScript_ES6/typescript/typescript/app.js
@@ -1,3 +1,14 @@
 "use strict";
 (function () {
+    //parameters order: Obligatory, optionals, default value
+    function activate(who, moment, object) {
+        if (object === void 0) { object = 'batsignal'; }
+        if (moment) {
+            console.log(who + " activate the " + object + " at the " + moment);
+        }
+        else {
+            console.log(who + " activate the " + object);
+        }
+    }
+    activate('Gordon', 'night');
 })();
