27871df2a333d1a0a7bbc502f4381aa991032aa6
Rafael <rortizr@unbosque.edu.co>
2020-09-12 14:00:21 -0500

    ngIf y ngFor

--- Seccion3:HelloWorld/01-hello-world/src/app/components/body/body.component.ts
+++ Seccion3:HelloWorld/01-hello-world/src/app/components/body/body.component.ts
@@ -5,5 +5,13 @@ import {Component} from '@angular/core'
     templateUrl: `./body.component.html`
 })
 export class BodyComponent{
-    
+
+    show = true;
+
+    raze: any = {
+        message: 'A big power requires big responsibility',
+        author: 'Ben Parker '
+    }
+
+    characters: string[] = ['spiderman','venom','octopus'];
 }
